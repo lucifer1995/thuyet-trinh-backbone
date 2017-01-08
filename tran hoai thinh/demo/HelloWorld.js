5d684969c0de6d4e3e0614b371e4b96f64866884
lucifer1995 <leesynz95@gmail.com>
2017-01-08 12:50:37 +0700

    Gioi thieu backbone

---
+++ tran hoai thinh/demo/HelloWorld.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+var AppView = Backbone.View.extend({
+	el: '#container',
+	initialize: function() {
+		this.render();
+	},
+	render: function() {
+		this.$el.html("Hello World !!!");
+	}
+});
+var appView = new AppView();
